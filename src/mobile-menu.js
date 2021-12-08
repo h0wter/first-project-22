2410512764ef8c6257b79c4b708fbf4fc9210b68
Mariia-Petlovana-91 <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2021-12-08 21:48:09 +0300

    finish work

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -1,14 +1,23 @@
 (() => {
   const refs = {
-     
+    
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
+closeHomeMenuBtn: document.querySelector('[data-hone-menu-close]'),
+closeAboutMenuBtn: document.querySelector('[data-about-menu-close]'),
+closeHowMenuBtn: document.querySelector('[data-how-menu-close]'),
+closeOurMenuBtn: document.querySelector('[data-our-menu-close]'),
+    closeContMenuBtn: document.querySelector('[data-cont-menu-close]'),
     menu: document.querySelector('[data-menu]'),
   };
-
+ 
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
-
+  refs.closeHomeMenuBtn.addEventListener('click', toggleModal);
+  refs.closeAboutMenuBtn.addEventListener('click', toggleModal);
+refs.closeHowMenuBtn.addEventListener('click', toggleModal);
+refs.closeOurMenuBtn.addEventListener('click', toggleModal);
+  refs.closeContMenuBtn.addEventListener('click', toggleModal);
   function toggleModal() {
     refs.menu.classList.toggle('is-open');
   }
