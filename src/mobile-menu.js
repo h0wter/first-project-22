addbe3f5768a6fe5d61e89fc3dff3d0f33575806
Mariia-Petlovana-91 <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2021-12-08 21:23:19 +0300

    bay now відкриває і в мобілці

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -1,5 +1,6 @@
 (() => {
   const refs = {
+     
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
